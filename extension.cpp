4bfaac2e06b52389b0694e91c4b1a0e7a1f688b4
Dots <dotvnl@gmail.com>
2026-03-06 21:40:19 +0200

    add win build

--- extension.cpp
+++ extension.cpp
@@ -8,10 +8,17 @@
 #include "extension.h"
 #include <string.h>
 #include <stdint.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <dlfcn.h>
-#include <link.h>
+
+#ifdef _WIN32
+  #include <windows.h>
+  #include <psapi.h>
+  #pragma comment(lib, "psapi.lib")
+#else
+  #include <sys/mman.h>
+  #include <unistd.h>
+  #include <dlfcn.h>
+  #include <link.h>
+#endif
 
 CCSGOSteamFix g_SteamFix;
 SMEXT_LINK(&g_SteamFix);
@@ -21,16 +28,26 @@ static const unsigned char g_sigLinux[]  = { 0xFF, 0x24, 0x85, 0x00, 0x00, 0x00,
 static const unsigned char g_maskLinux[] = { 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF };
 static const size_t g_sigLinuxLen = sizeof(g_sigLinux);
 
+// Windows: FF 24 85 ?? ?? ?? ?? FF 75 ?? 68
+static const unsigned char g_sigWin[]    = { 0xFF, 0x24, 0x85, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x75, 0x00, 0x68 };
+static const unsigned char g_maskWin[]   = { 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF };
+static const size_t g_sigWinLen = sizeof(g_sigWin);
+
 static bool      g_patched    = false;
 static uint32_t  g_origValue  = 0;
 static uint32_t *g_patchAddr  = NULL;
 
 static bool SetMemoryWritable(void *addr, size_t len)
 {
+#ifdef _WIN32
+    DWORD oldProt;
+    return VirtualProtect(addr, len, PAGE_EXECUTE_READWRITE, &oldProt) != 0;
+#else
     long pageSize = sysconf(_SC_PAGESIZE);
     uintptr_t base = (uintptr_t)addr & ~((uintptr_t)pageSize - 1);
     size_t totalLen = ((uintptr_t)addr + len) - base;
     return mprotect((void *)base, totalLen, PROT_READ | PROT_WRITE | PROT_EXEC) == 0;
+#endif
 }
 
 static void *SigScan(void *base, size_t len, const unsigned char *sig, const unsigned char *mask, size_t sigLen)
@@ -60,6 +77,30 @@ struct ModuleInfo_t
     void  *base;
     size_t size;
 };
+
+#ifdef _WIN32
+
+static ModuleInfo_t GetEngineModule()
+{
+    ModuleInfo_t info;
+    info.base = NULL;
+    info.size = 0;
+
+    HMODULE hMod = GetModuleHandleA("engine.dll");
+    if (!hMod)
+        return info;
+
+    MODULEINFO mi;
+    memset(&mi, 0, sizeof(mi));
+    if (GetModuleInformation(GetCurrentProcess(), hMod, &mi, sizeof(mi)))
+    {
+        info.base = mi.lpBaseOfDll;
+        info.size = mi.SizeOfImage;
+    }
+    return info;
+}
+
+#else
 
 static ModuleInfo_t g_engineInfo;
 
@@ -93,6 +134,8 @@ static ModuleInfo_t GetEngineModule()
     return g_engineInfo;
 }
 
+#endif
+
 static void LogMsg(const char *fmt, ...)
 {
     char buf[512];
@@ -109,14 +152,44 @@ bool CCSGOSteamFix::SDK_OnLoad(char *error, size_t maxlength, bool late)
     ModuleInfo_t eng = GetEngineModule();
     if (!eng.base || !eng.size)
     {
-        snprintf(error, maxlength, "[APPID-Patch] failed to locate engine module");
+        snprintf(error, maxlength, "[APPID] failed to locate engine module");
         return false;
     }
+
+#ifdef _WIN32
+
+    void *hit = SigScan(eng.base, eng.size, g_sigWin, g_maskWin, g_sigWinLen);
+    if (!hit)
+    {
+        snprintf(error, maxlength, "[APPID] signature not found (Windows)");
+        return false;
+    }
+
+    uint32_t jtAddr = *(uint32_t *)((unsigned char *)hit + 3);
+    uint32_t *jt    = (uint32_t *)jtAddr;
+
+    unsigned char *jaInstr = (unsigned char *)hit - 6;
+    int32_t rel32 = *(int32_t *)(jaInstr + 2);
+    uint32_t successVA = (uint32_t)(uintptr_t)(jaInstr + 6) + rel32;
+
+    g_patchAddr = &jt[3];
+    g_origValue = jt[3];
+
+    if (!SetMemoryWritable(g_patchAddr, sizeof(uint32_t)))
+    {
+        snprintf(error, maxlength, "[APPID] VirtualProtect failed");
+        return false;
+    }
+
+    *g_patchAddr = successVA;
+    g_patched = true;
+
+#else
 
     void *hit = SigScan(eng.base, eng.size, g_sigLinux, g_maskLinux, g_sigLinuxLen);
     if (!hit)
     {
-        snprintf(error, maxlength, "[APPID-Patch] signature not found (Linux)");
+        snprintf(error, maxlength, "[APPID] signature not found (Linux)");
         return false;
     }
 
@@ -128,14 +201,16 @@ bool CCSGOSteamFix::SDK_OnLoad(char *error, size_t maxlength, bool late)
 
     if (!SetMemoryWritable(g_patchAddr, sizeof(uint32_t)))
     {
-        snprintf(error, maxlength, "[APPID-Patch] mprotect failed");
+        snprintf(error, maxlength, "[APPID] mprotect failed");
         return false;
     }
 
     *g_patchAddr = jt[0];
     g_patched = true;
 
-    LogMsg("[APPID-Patch] engine patched!");
+#endif
+
+    LogMsg("[APPID] engine patched!");
     return true;
 }
 
@@ -146,6 +221,6 @@ void CCSGOSteamFix::SDK_OnUnload()
         SetMemoryWritable(g_patchAddr, sizeof(uint32_t));
         *g_patchAddr = g_origValue;
         g_patched = false;
-        LogMsg("[APPID-Patch] engine restored to original state");
+        LogMsg("[APPID] engine restored to original state");
     }
 }
